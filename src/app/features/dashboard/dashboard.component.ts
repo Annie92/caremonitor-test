700b7d5275cadbce998cb007dd3400aeaa24be95
anniep <annie92p@gmail.com>
2025-05-28 13:16:30 +0530

    sample Usage of ngx-cookie-service in dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
+  providers: [CookieService],
 })
 export class DashboardComponent {
+  cookieValue: string = '';
   username: string = 'Annie Pappachan'; 
+  cookieService = inject(CookieService);
+  constructor(){
+    this.cookieService.set('Test', 'Hello World');
+    this.cookieValue = this.cookieService.get('Test');
+    console.log('Cookie Value:', this.cookieValue);
+  }
+
 }
